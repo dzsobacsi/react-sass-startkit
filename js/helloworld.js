d3949044f35b8877c15ace0533f3f70b712fce84
dzsobacsi <tberki77@gmail.com>
2016-05-15 21:02:06 +0200

    initial commit

---
+++ js/helloworld.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloForm from './HelloForm';
+
+ReactDOM.render(<HelloForm />, document.getElementsByTagName('body')[0]);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    entry: {
+        helloWorld: [
+          'webpack-dev-server/client?http://localhost:8080',
+          'webpack/hot/only-dev-server',
+          './js/helloworld.js'
+        ]
+    },
+    output: {
+        filename: 'public/[name].js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'react-hot!babel',
+          exclude: /node_modules/
+        }, {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          exclude: /node_modules/
+        }/*,
+        {
+          test: /\.sass$/,
+          loaders: ['style', 'css', 'sass'],
+          exclude: /node_modules/
+        }*/
+      ]
+    }
+};
